f2f93176648449b18a523e6373a11794948d5030
René Müller <rmueller@s-u-a.net>
2006-04-30 19:54:27 +0000

    Fit to skins.unpacked.
    git-svn-id: file:///srv/svn/sua-extras@9 fa721403-a712-0410-b0d0-f7f67a106baa

--- skins.php
+++ skins.php
@@ -2,19 +2,19 @@
 	include('include.php');
 	
 	$skins = array();
-	$dh = opendir('skins');
+	$dh = opendir('skins.unpacked');
 	while(($dir = readdir($dh)) !== false)
 	{
 		if($dir[0] == '#' || $dir[0] == '.') continue;
-		if(!is_dir('skins/'.$dir) || !is_file('skins/'.$dir.'/.name')) continue;
-		$skins[$dir] = file_get_contents('skins/'.$dir.'/.name');
+		if(!is_dir('skins/'.$dir) || !is_file('skins.unpacked/'.$dir.'/.name')) continue;
+		$skins[$dir] = file_get_contents('skins.unpacked/'.$dir.'/.name');
 	}
 	closedir($dh);
 	
 	if(isset($_GET['skin']) && isset($skins[$_GET['skin']]))
 	{
-		if(!is_file('cache/skins/'.$_GET['skin'].'.tar.bz2') || filemtime('cache/skins/'.$_GET['skin'].'.tar.bz2') < last_filemtime('skins/'.$_GET['skin']))
-			exec('tar -cjfh cache/skins/'.$_GET['skin'].'.tar.bz2 skins/'.$_GET['skin']);
+		if(!is_file('cache/skins/'.$_GET['skin'].'.tar.bz2') || filemtime('cache/skins/'.$_GET['skin'].'.tar.bz2') < last_filemtime('skins.unpacked/'.$_GET['skin']))
+			exec('tar -cjfh cache/skins/'.$_GET['skin'].'.tar.bz2 skins.unpacked/'.$_GET['skin']);
 		header('Location: '.s_root.'/cache/skins/'.$_GET['skin'].'.tar.bz2', true, 307);
 	}
 
