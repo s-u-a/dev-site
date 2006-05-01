23a61cbf8cc75cabce43611f28dd316d54a23e1a
René Müller <rmueller@s-u-a.net>
2006-05-01 10:14:44 +0000

    Corrected redirection in bugs.php.
    git-svn-id: file:///srv/svn/sua-extras@18 fa721403-a712-0410-b0d0-f7f67a106baa

--- bugs.php
+++ bugs.php
@@ -1,12 +1,12 @@
 <?php
 	include('include.php');
 	
-	header('Location: https://bugs.dev.s-u-a.net/', true, 303);
+	header('Location: https://dev.s-u-a.net/bugs/', true, 303);
 
 	$gui->title = $lang->getEntry('navigation', 'bugs');
 	$gui->htmlHead();
 ?>
-<p><?=$lang->getEntry('bugs', 'redirect', 'https://bugs.s-u-a.net/')?></p>
+<p><?=$lang->getEntry('bugs', 'redirect', 'https://dev.s-u-a.net/bugs/')?></p>
 <?php
 	$gui->htmlFoot();
 	exit(0);
