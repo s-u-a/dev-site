c1bde26b757b8b306b19f918ff4c52f32779d58b
René Müller <rmueller@s-u-a.net>
2006-05-01 13:50:21 +0000

    Created news.
    git-svn-id: file:///srv/svn/sua-extras@33 fa721403-a712-0410-b0d0-f7f67a106baa

--- news.php
+++ news.php
@@ -3,8 +3,43 @@
 
 	$gui->title = $lang->getEntry('navigation', 'news');
 	$gui->htmlHead();
+	
+	$language = $lang->getSelectedLanguage();
+	
+	$news = array();
+	if(is_dir('newsdata') && is_readable('newsdata') && is_dir('newsdata/'.$language) && is_readable('newsdata/'.$language))
+	{
+		$dh = opendir('newsdata/'.$language);
+		while(($file = readdir($dh)) !== false)
+		{
+			if(!is_file('newsdata/'.$language.'/'.$file) || !is_readable('newsdata/'.$language.'/'.$file) || 'newsdata/'.$language.'/'.$file[0] == '.' || 'newsdata/'.$language.'/'.$file[0] == '#') continue;
+			$news[$file] = filemtime('newsdata/'.$language.'/'.$file);
+		}
+		closedir($dh);
+	}
+	arsort($news, SORT_NUMERIC);
+	
+	if($news)
+	{
+		foreach($news as $filename=>$filemtime)
+		{
+			$one_news = parseNewsXML('newsdata/'.$language.'/'.$filename);
+			$one_news['content'] = str_replace("\n", "\n\t", $one_news['content']);
 ?>
+<div class="news">
+	<h3><?=$one_news['title']?></h3>
+	<?=$one_news['content']?>
+	<cite><?=$one_news['author'].', '.date('Y-m-d, H:i:s', $filemtime)?></cite>
+</div>
 <?php
+		}
+	}
+	else
+	{
+?>
+<p class="error"><?=$lang->getEntry('news', 'no-news')?></p>
+<?php
+	}
 	$gui->htmlFoot();
 	exit(0);
 ?>
