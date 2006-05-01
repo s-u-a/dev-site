1debc6668d258d97e5320faa743e6af4194cdfc4
Candid Dauth <cdauth@cdauth.de>
2006-05-01 00:03:33 +0000

    Created design.
    git-svn-id: file:///srv/svn/sua-extras@13 fa721403-a712-0410-b0d0-f7f67a106baa

--- include/020_gui.php
+++ include/020_gui.php
@@ -11,24 +11,25 @@
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="de">
 	<head>
-		<title><?=(strlen(trim($this->title)) > 0) ? htmlspecialchars(trim($this->title)).' – ' : ''?><?=$lang->getEntry('general', 'title')?></title>
+		<title><?=(strlen(trim($this->title)) > 0) ? htmlspecialchars(strip_tags(trim($this->title))).' – ' : ''?><?=strip_tags($lang->getEntry('general', 'title'))?></title>
 		<style type="text/css">
 			@import url(<?=h_root?>/style.css);
 		</style>
 	</head>
 	<body>
-		<h1><?=$lang->getEntry('general', 'title')?></h1>
+		<h1><span><?=$lang->getEntry('general', 'title')?></span></h1>
 		<ol id="navigation">
-			<li><a href="<?=h_root?>/"><?=$lang->getEntry('navigation', 'index')?></a></li>
-			<li><a href="<?=h_root?>/news"><?=$lang->getEntry('navigation', 'news')?></a></li>
-			<li><a href="<?=h_root?>/credits"><?=$lang->getEntry('navigation', 'credits')?></a></li>
-			<li><a href="<?=h_root?>/skins"><?=$lang->getEntry('navigation', 'skins')?></a></li>
-			<li><a href="<?=h_root?>/download"><?=$lang->getEntry('navigation', 'download')?></a></li>
-			<li><a href="<?=h_root?>/forum"><?=$lang->getEntry('navigation', 'forum')?></a></li>
-			<li><a href="<?=h_root?>/bugs"><?=$lang->getEntry('navigation', 'bugs')?></a></li>
-			<li><a href="<?=h_root?>/websvn"><?=$lang->getEntry('navigation', 'websvn')?></a></li>
-			<li><a href="<?=h_root?>/faq"><?=$lang->getEntry('navigation', 'faq')?></a></li>
+			<li<?=_GUI_checkActive('index')?' class="active"':''?>><a href="<?=h_root?>/"><?=$lang->getEntry('navigation', 'index')?></a></li>
+			<li<?=_GUI_checkActive('news')?' class="active"':''?>><a href="<?=h_root?>/news"><?=$lang->getEntry('navigation', 'news')?></a></li>
+			<li<?=_GUI_checkActive('credits')?' class="active"':''?>><a href="<?=h_root?>/credits"><?=$lang->getEntry('navigation', 'credits')?></a></li>
+			<li<?=_GUI_checkActive('skins')?' class="active"':''?>><a href="<?=h_root?>/skins"><?=$lang->getEntry('navigation', 'skins')?></a></li>
+			<li<?=_GUI_checkActive('download')?' class="active"':''?>><a href="<?=h_root?>/download"><?=$lang->getEntry('navigation', 'download')?></a></li>
+			<li<?=_GUI_checkActive('forum')?' class="active"':''?>><a href="<?=h_root?>/forum"><?=$lang->getEntry('navigation', 'forum')?></a></li>
+			<li<?=_GUI_checkActive('bugs')?' class="active"':''?>><a href="<?=h_root?>/bugs"><?=$lang->getEntry('navigation', 'bugs')?></a></li>
+			<li<?=_GUI_checkActive('websvn')?' class="active"':''?>><a href="<?=h_root?>/websvn"><?=$lang->getEntry('navigation', 'websvn')?></a></li>
+			<li<?=_GUI_checkActive('faq')?' class="active"':''?>><a href="<?=h_root?>/faq"><?=$lang->getEntry('navigation', 'faq')?></a></li>
 		</ol>
+		<div id="content">
 <?php
 			if(strlen(trim($this->title)) > 0)
 			{
@@ -37,7 +38,6 @@
 <?php
 			}
 ?>
-		<div id="content">
 <?php
 			ob_start();
 			return true;
@@ -50,7 +50,7 @@
 			$content = ob_get_contents();
 			ob_end_clean();
 
-			print "\t\t\t".str_replace("\n", "\n\t\t\t", $content);
+			print "\t\t\t".str_replace("\n", "\n\t\t\t", $content)."\n";
 ?>
 		</div>
 	</body>
@@ -58,6 +58,11 @@
 <?php
 			return true;
 		}
+	}
+
+	function _GUI_checkActive($cat)
+	{
+		return ($_SERVER['PHP_SELF'] == h_root.'/'.$cat.'.php');
 	}
 
 	$gui = new GUI;
