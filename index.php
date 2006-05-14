9cc6ee57ce9b30e983a7983bc5efb8c3ef1d80c5
Candid Dauth <cdauth@cdauth.de>
2006-05-14 10:08:32 +0000

    Mailing lists work now.
    git-svn-id: file:///srv/svn/sua-extras@51 fa721403-a712-0410-b0d0-f7f67a106baa

--- index.php
+++ index.php
@@ -1,52 +1,58 @@
 <?php
 	include('include.php');
 
+	function mailinglist_action($to, $email)
+	{
+		return mail($to, '', '', 'From: '.$email);
+		
+		/*$ph = popen('/usr/bin/mlmmj-recieve', 'w');
+		if(!$ph) return false;
+		fwrite($ph, "From: ".$email."\r\n");
+		fwrite($ph, "To: ".$to."\r\n");
+		fwrite($ph, "\r\n");
+		pclose($ph);
+		return true;*/
+	}
+
 	$gui->title = $lang->getEntry('navigation', 'index');
 	$gui->htmlHead();
 ?>
 <p><?=$lang->getEntry('index', 'introduction')?></p>
 
-<?php
-	if(is_file('mlmmj.php') && is_readable('mlmmj.php'))
-	{
-?>
 <h3 id="mailinglist"><?=$lang->getEntry('index', 'mailinglist-heading')?></h3>
 <?php
-		if(isset($_GET['mlsuccess']))
+	if(isset($_POST['email']) && strlen(trim($_POST['email'])) > 0)
+	{
+		if(eregi("^[a-z0-9\._-]+".chr(64)."+[a-z0-9\._-]+\.+[a-z]{2,4}$", $_POST['email']) && mailinglist_action('sua-dev+'.((isset($_POST['unsubscribe']) && $_POST['unsubscribe']) ? 'unsubscribe' : 'subscribe').'@s-u-a.net', $_POST['email']))
 		{
-			if($_GET['mlsuccess'])
-			{
 ?>
-<p class="successful"><?=$lang->getEntry('mailinglist-successful')?></p>
+<p class="successful"><?=$lang->getEntry('index', 'mailinglist-successful')?></p>
 <?php
-			}
-			else
-			{
-?>
-<p class="error"><?=$lang->getEntry('mailinglist-error')?></p>
-<?php
-			}
 		}
 		else
 		{
 ?>
-<p><?=$lang->getEntry('index', 'mailinglist-text')?></p>
+<p class="error"><?=$lang->getEntry('index', 'mailinglist-error')?></p>
 <?php
 		}
+	}
+	else
+	{
 ?>
-<form action="mlmmj.php" method="post">
+<p><?=$lang->getEntry('index', 'mailinglist-text')?></p>
+<?php
+	}
+?>
+<form action="<?=h_root?>/" method="post">
 	<dl>
 		<dt><label for="i-email"><?=$lang->getEntry('index', 'mailinglist-email')?></label></dt>
 		<dd><input type="text" name="email" id="i-email" /></dd>
 	</dl>
 	<ul>
-		<li><button name="job" type="submit" value="subscribe"><?=$lang->getEntry('index', 'mailinglist-subscribe')?></button><input name="mailinglist" type="hidden" value="sua-dev@s-u-a.net" /><input name="redirect_failure" type="hidden" value="http://<?=htmlspecialchars($_SERVER['HTTP_HOST'].h_root.'/?mlsuccess=0')?>#mailinglist" /><input name="redirect_success" type="hidden" value="http://<?=htmlspecialchars($_SERVER['HTTP_HOST'].h_root.'/?mlsuccess=0')?>#mailinglist" /></li>
-		<li><button name="job" type="submit" value="unsubcribe"><?=$lang->getEntry('index', 'mailinglist-unsubscribe')?></button></li>
+		<li><button type="submit"><?=$lang->getEntry('index', 'mailinglist-subscribe')?></button></li>
+		<li><button name="unsubscribe" type="submit" value="1"><?=$lang->getEntry('index', 'mailinglist-unsubscribe')?></button></li>
 	</ul>
 </form>
-<?php
-	}
-?>
 
 <h3 id="chat"><?=$lang->getEntry('index', 'chat-heading')?></h3>
 <p><?=$lang->getEntry('index', 'chat-text', 'irc.epd-me.net', 'sua-dev')?></p>
