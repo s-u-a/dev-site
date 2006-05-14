2f308af7127551f81f7a184ee83b3a0c444b6030
Candid Dauth <cdauth@cdauth.de>
2006-05-14 14:01:48 +0000

    Persons.
    git-svn-id: file:///srv/svn/sua-extras@53 fa721403-a712-0410-b0d0-f7f67a106baa

--- credits.php
+++ credits.php
@@ -4,6 +4,22 @@
 	$gui->title = $lang->getEntry('navigation', 'credits');
 	$gui->htmlHead();
 ?>
+<dl>
+	<dt><a href="mailto:webmaster@s-u-a.net">Candid Dauth</a> (<a href="http://cdauth.de/">http://cdauth.de/</a>)</dt>
+	<dd><?=$lang->getEntry('credits', 'cdauth-work')?></dd>
+
+	<dt><a href="mailto:rmueller@s-u-a.net">rmueller</a> (<a href="http://rmueller.info/">http://rmueller.info/</a>)</dt>
+	<dd><?=$lang->getEntry('credits', 'rmueller-work')?></dd>
+
+	<dt><a href="mailto:soltari@s-u-a.net">Soltari</a></dt>
+	<dd><?=$lang->getEntry('credits', 'soltari-work')?></dd>
+
+	<dt>Geki</dt>
+	<dd><?=$lang->getEntry('credits', 'geki-work')?></dd>
+
+	<dt><a href="mailto:barade@s-u-a.net">Barade</a></dt>
+	<dd><?=$lang->getEntry('credits', 'barade-work')?></dd>
+</dl>
 <?php
 	$gui->htmlFoot();
 	exit(0);
